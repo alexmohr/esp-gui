2078dd261a5366e4b273d42632af942681bc5b1e
Alexander Mohr <alexander.m.mohr@mercedes-benz.com>
2022-07-03 18:47:58 +0200

    config: add lists and button support
    Signed-off-by: Alexander Mohr <alexander.m.mohr@mercedes-benz.com>

--- examples/src/main.cpp
+++ examples/src/main.cpp
@@ -14,6 +14,7 @@ yal::appender::ArduinoSerial<HardwareSerial> m_serialAppender(&m_logger, &Serial
 
 esp_gui::Configuration m_config;
 esp_gui::WebServer m_server(80, "demo", m_config);
+esp_gui::WifiManager m_wifiMgr(m_config, m_server);
 String m_powerFoobar = "power_foobar";
 String m_powerUsage = "power_usage";
 String m_myConfig = "power_int";
@@ -21,7 +22,7 @@ String m_myConfig = "power_int";
 void setup() {
   m_serialAppender.begin(115200);
   m_logger.log(yal::Level::INFO, "Running setup");
-  m_logger.setLevel(yal::Level::TRACE);
+  yal::Logger::setLevel(yal::Level::TRACE);
   m_config.setup();
 
   m_server.setPageTitle("ESP-GUI Demo");
@@ -29,7 +30,7 @@ void setup() {
   m_config.setValue(m_powerFoobar, "foobar");
   m_config.setValue(m_powerUsage, 42);
 
-  std::vector<esp_gui::Element> elements;
+  std::vector<std::any> elements;
   elements.emplace_back(
     esp_gui::Element(esp_gui::ElementType::INT, String("Power Usage"), m_powerUsage));
   elements.emplace_back(
@@ -38,14 +39,17 @@ void setup() {
     esp_gui::Element(esp_gui::ElementType::INT, String("Config Int"), m_myConfig));
   esp_gui::Container powerUsage("Power usage", std::move(elements));
 
+  // todo support more than 1 button
+
   m_server.addContainer(std::move(powerUsage));
-  esp_gui::WifiManager wifiMgr(m_config, m_server);
-  wifiMgr.setup(false);
+
+  m_wifiMgr.setup(false);
 
   m_server.setup(m_config.value<String>("wifi_hostname"));
 }
 
 void loop() {
+  m_wifiMgr.loop();
   delay(1000);
   int currentUsage = m_config.value<int>(m_powerUsage);
   m_config.setValue(m_powerUsage, currentUsage + 1);
